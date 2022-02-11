e5583026208f20d8c5907d936dd6bc2ddb9b9d66
Latham Fell <lathamfell@gmail.com>
2022-02-11 05:05:56 -0800

    Update 31_Max-Stacks.py

--- stack/31_Max-Stacks.py
+++ stack/31_Max-Stacks.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python
 # coding=utf-8
 '''
-__author__ = 'sunp'
-__date__ = '2019/1/26'
+__author__ = 'lf'
+__date__ = '2022/02/11'
 
 Implement a LIFO stack that has a push(), pop(), and max() function, where max() returns the maximum value in the stack. All of these functions should run in O(1) time.
 '''
 
 
-class Node(object):
-    def __init__(self, val, next=None, pre_max=None):
-        self.val = val
-        self.next = next
-        self.pre_max = pre_max
+class MaxStack:
+	def __init__(self):
+		self.max = None
+		self.head = Node()
+
+	def push(self, val):
+		n = Node(val)
+		n.next = self.head.next
+		self.head.next = n
+		if self.max is None or val > self.max:
+			# new max
+			n.old_max = self.max
+			self.max = val
+
+	def pop(self):
+		ret_node = self.head.next
+		self.head.next = ret_node.next
+		if ret_node.old_max is not None:
+			self.max = ret_node.old_max
+		return ret_node.val
+
+	def max(self):
+		return self.max
 
 
-class MaxStack(object):
-    '''
-    LinkNode: add/remove in O(1) time
-    '''
-    def __init__(self):
-        self.stack = None
-        self.cur_max = None
+class Node:
+	def __init__(self, val=None):
+		self.val = val
+		self.next = None
+		self.old_max = None
 
-    def push(self, x):
-        p = Node(x)
-        p.next = self.stack
-        # self.stack == head
-        self.stack = p
-
-        p.pre_max = self.cur_max
-        if not self.cur_max or p.val > self.cur_max.val:
-            self.cur_max = p
-
-    def pop(self):
-        if not self.stack:
-            raise ValueError
-
-        head = self.stack
-        self.stack = head.next
-        self.cur_max = head.pre_max
-        return head.val
-
-    def max(self):
-        if not self.cur_max:
-            raise ValueError
-
-        return self.cur_max.val
 
 
 if __name__ == '__main__':
